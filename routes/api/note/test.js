46d649515363d926a4e9de5dc0db80b14904c4f3
wangchenglin <wcl58981298@gmail.com>
2016-06-14 23:54:25 +0800

    fixed test

---
+++ routes/api/note/test.js
@@ -0,0 +1,8 @@
+module.exports = {
+  GET: function *() {
+/*
+get test note
+ */
+    this.body = 'test note'
+  }
+}
---
+++ routes/api/user/test.js
@@ -0,0 +1,5 @@
+module.exports = {
+  GET: function *() {
+    this.body = 'test user'
+  }
+}
