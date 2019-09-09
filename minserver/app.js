2f3d4dcb2cc221b2e1245d02663a640594b51a86
Clay <wcl58981298@gmail.com>
2019-09-09 18:12:44 +0800

    add minserver

--- minserver/app.js
+++ minserver/app.js
@@ -7,26 +7,26 @@ const app = express();
 app.use(bodyparser.urlencoded({ extende:true }));
 app.use(bodyparser.json());
 
-app.use("*", function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  if (req.method === 'OPTIONS') {
-    res.send(200)
-  } else {
-    next()
-  }
-});
+// app.use("*", function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+//   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//   if (req.method === 'OPTIONS') {
+//     res.send(200)
+//   } else {
+//     next()
+//   }
+// });
 
-app.get('/', (req, res) => {
+app.get('/dictadmin/', (req, res) => {
   res.send(fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8'));
 });
 
-app.get('/dict.json', (req, res) => {
+app.get('/dictadmin/dict.json', (req, res) => {
   res.send(fs.readFileSync(path.resolve(__dirname, '../dict.json'), 'utf-8'));
 });
 
-app.get('/backup', (req, res) => {
+app.get('/dictadmin/backup', (req, res) => {
   const text = fs.readFileSync(path.resolve(__dirname, '../dict.json'), 'utf-8');
   fs.writeFileSync(path.resolve(__dirname, `../dict-${new Date().getTime()}.json`), text, 'utf-8');
   res.send({
@@ -35,7 +35,7 @@ app.get('/backup', (req, res) => {
   });
 });
 
-app.post('/', (req, res) => {
+app.post('/dictadmin', (req, res) => {
   try {
     fs.writeFileSync(path.resolve(__dirname, '../dict.json'), req.body.json, 'utf-8');
     res.send({
