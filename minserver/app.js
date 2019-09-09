5d3d31b75ef44bc1d3709074589e4c4897e4e368
Clay <wcl58981298@gmail.com>
2019-09-09 17:34:39 +0800

    fix

--- minserver/app.js
+++ minserver/app.js
@@ -7,6 +7,17 @@ const app = express();
 app.use(bodyparser.urlencoded({ extende:true }));
 app.use(bodyparser.json());
 
+app.use("*", function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  if (req.method === 'OPTIONS') {
+    res.send(200)
+  } else {
+    next()
+  }
+});
+
 app.get('/', (req, res) => {
   res.send(fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8'));
 });
